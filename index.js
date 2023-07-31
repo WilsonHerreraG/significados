b2d45fd7b6677f7f3b98d1f279f857c30c2aa604
WilsonHerreraG <122500352+WilsonHerreraG@users.noreply.github.com>
2023-07-31 00:07:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,258 @@
+function mostrar(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion1').style.display='none'; 
+}
+function ocultar(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion1').style.display='block';
+}
+function mostrar2(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion2').style.display='none'; 
+}
+function ocultar2(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion2').style.display='block';
+}
+function mostrar3(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    miDiv.style.height = '890px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion3').style.display='none';   
+}
+function ocultar3(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '500px';
+    miDiv.style.height = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion3').style.display='block';
+}
+function mostrar4(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    miDiv.style.height = '890px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion4').style.display='none';
+}
+function ocultar4(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '600px';
+    miDiv.style.height = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion4').style.display='block';
+}
+function ocultar5(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '600px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion5').style.display='block';
+}
+function mostrar5(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion5').style.display='none';
+}
+function ocultar6(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '720px';
+    
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion6').style.display='block';
+}
+function mostrar6(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion6').style.display='none';
+}
+function ocultar7(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion7').style.display='block'; 
+}
+function mostrar7(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion7').style.display='none';
+}
+function ocultar8(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    miDiv.style.height = '750px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion8').style.display='block';
+}
+function mostrar8(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    miDiv.style.height = '890px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion8').style.display='none';
+}
+function ocultar9(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion9').style.display='block';
+}
+function mostrar9(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion9').style.display='none';
+}
+function ocultar10(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion10').style.display='block';
+}
+function mostrar10(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion10').style.display='none';
+}
+function ocultar11(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion11').style.display='block';
+}
+function mostrar11(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion11').style.display='none';
+}
+function ocultar12(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion12').style.display='block';
+}
+function mostrar12(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion12').style.display='none';
+}
+function ocultar13(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion13').style.display='block';   
+}
+function mostrar13(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion13').style.display='none';
+}
+function ocultar14(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion14').style.display='block';
+}
+function mostrar14(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion14').style.display='none';
+}
+function ocultar15(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion15').style.display='block';   
+}
+function mostrar15(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion15').style.display='none';
+}
+function mostrar01(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '480px';
+    miDiv.style.height = '890px';
+    document.getElementById('form1').style.display='block';
+    document.getElementById('opcion01').style.display='none';
+}
+function ocultar01(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '300px';
+    miDiv.style.height = '500px';
+    document.getElementById('form1').style.display='none';
+    document.getElementById('opcion01').style.display='block'; 
+}
+function mostrar02(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '300px';
+    miDiv.style.height = '500px';
+    document.getElementById('opcion01').style.display='block';
+    document.getElementById('opcion02').style.display='none';
+}
+function ocultar02(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    miDiv.style.height = '900px';
+    document.getElementById('opcion01').style.display='none';
+    document.getElementById('opcion02').style.display='block'; 
+}
+function mostrar03(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '300px';
+    miDiv.style.height = '500px';
+    document.getElementById('opcion01').style.display='block';
+    document.getElementById('opcion03').style.display='none';
+}
+function ocultar03(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    miDiv.style.height = '900px';
+    document.getElementById('opcion01').style.display='none';
+    document.getElementById('opcion03').style.display='block'; 
+}
+function mostrar04(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '300px';
+    miDiv.style.height = '500px';
+    document.getElementById('opcion01').style.display='block';
+    document.getElementById('opcion04').style.display='none';
+}
+function ocultar04(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    miDiv.style.height = '900px';
+    document.getElementById('opcion01').style.display='none';
+    document.getElementById('opcion04').style.display='block'; 
+}
+function mostrar05(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '300px';
+    miDiv.style.height = '500px';
+    document.getElementById('opcion01').style.display='block';
+    document.getElementById('opcion05').style.display='none';
+}
+function ocultar05(){
+    const miDiv = document.getElementById('conte'); 
+    miDiv.style.width = '700px';
+    miDiv.style.height = '900px';
+    document.getElementById('opcion01').style.display='none';
+    document.getElementById('opcion05').style.display='block'; 
+}
+
